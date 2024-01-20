1e5600d588531ad04cd0ff2337de1cb9bede6aca
Carlos <carlosalonso9797@gmail.com>
2024-01-20 20:28:15 +0000

    ya esta hecho

--- src/main/java/software/ulpgc/kata3/Main.java
+++ src/main/java/software/ulpgc/kata3/Main.java
@@ -1,7 +1,7 @@
 package software.ulpgc.kata3;
 
 import software.ulpgc.kata3.swing.MainFrame;
-
+//kata 3 histograma
 public class Main {
     public static void main(String[] args) {
         Histogram histogram = new Histogram("Cantidad de juegos que se compran en una muestra escogida entre amigos", "Intervalo", "Cantidad", new double[]{6,9,4,15,10,3,6,7,12,2}, 4);
